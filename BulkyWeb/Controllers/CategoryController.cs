d7fe05bd96f452e4cab6f6cde3a179c1996d4d08
WenhaoLu-hub <s3810097@student.rmit.edu.au>
2023-08-03 17:30:31 +0800

    complete crud operation on category page

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -30,14 +30,78 @@ public class CategoryController : Controller
     {
         if (category.Name == category.DisplayOrder.ToString())
         {
-            ModelState.AddModelError(nameof(category.Name),"Name and Display Order cannot be the same.");
+            ModelState.AddModelError(nameof(category.Name), "Name and Display Order cannot be the same.");
         }
+
         if (ModelState.IsValid)
         {
             _context.Categories.Add(category);
             _context.SaveChanges();
+            TempData["success"] = "Category created successfully.";
             return RedirectToAction(nameof(Index));
         }
+
         return View();
+    }
+
+    public IActionResult Edit(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+
+        var category = _context.Categories.FirstOrDefault(x => x.CategoryId == id);
+        if (category is null)
+        {
+            return NotFound();
+        }
+
+        return View(category);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(Category category)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+            TempData["success"] = "Category updated successfully.";
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View();
+    }
+
+
+    public IActionResult Delete(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+
+        var category = _context.Categories.FirstOrDefault(x => x.CategoryId == id);
+        if (category is null)
+        {
+            return NotFound();
+        }
+
+        return View(category);
+    }
+
+    [HttpPost,ActionName("Delete")]
+    public IActionResult DeletePost(int? id)
+    {
+        var category = _context.Categories.Find(id);
+        if (category is null)
+        {
+            return NotFound();
+        }
+        _context.Categories.Remove(category);
+        _context.SaveChanges();
+        TempData["success"] = "Category deleted successfully.";
+        return RedirectToAction(nameof(Index));
     }
 }
